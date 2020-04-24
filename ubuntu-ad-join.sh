93bd7b70fa413a491bdba4b2cffded5e82501ce4
Joe Pat <joewpat@gmail.com>
2020-04-23 21:14:30 -0400

    Update ubuntu-ad-join.sh

--- ubuntu-ad-join.sh
+++ ubuntu-ad-join.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#Ubuntu 18 quick config script by joe. make it executable (chmod +x thisfile) and run it as root
+#Ubuntu 18.04 Active Directory join script
+#make this script executable and run as root to join an Ubuntu machine to a Windows domain
+#please be sure to look over the configuration files and modify them to suit your domain
 
 echo "The next step will install kerberos packages and needs user input. enter the domain <DOMAIN.LOCAL> IN ALL CAPS. That part is important"
 
@@ -8,11 +10,6 @@ read -p "Press Enter to continue"
 #dependency installs
 apt install -y krb5-user samba sssd chrony openssh-server cifs-utils
 #user will be asked for the domain name at this time(example.tld)
-
-#mount the file share. mine are stored on a windows 2016 file server so I mount it with CIFS:
-sudo mount -t cifs '<path to share with configs>' /mnt -o user=administrator@example.tld
-#copy config files. 
-
 
 echo "copying config files"
 
